33958e791ca7c579fcb3fe393678c1c5a7800946
Harikrishnanks312 <118618866+Harikrishnanks312@users.noreply.github.com>
2023-07-26 18:56:02 +0530

    cors

--- index.js
+++ index.js
@@ -22,14 +22,11 @@ const { log } = require('console');
 app.use('/admin', adminRoute)
 
 const server = app.listen(3000, () => {
-  console.log(`Server is running on port 8000.`);
+  console.log(`Server is running on port 3000.`);
 });
 
 const io = require('socket.io')(server, {
-  pingTimeout: 60000,
-  cors: {
-    origin: "http://localhost:5173"
-  }
+  cors:true
 })
 
 io.on("connection", (socket) => {
